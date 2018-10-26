c5df8fd1812e943d99615b70873374b4e192b9f5
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-10-26 22:55:15 +0200

    projekt

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('dodaj').addEventListener('click', function() {
+document.getElementById('tekst').innerHTML = "Akademia108!";
+});
+document.getElementById('usun').addEventListener('click', function(){
+    document.getElementById('tekst').innerHTML = '';
+});
